72805ec4702850400ae4f0581d18452d36db4b2c
Sana Sheraz <sanasheraz.dev@gmail.com>
2025-06-11 01:04:00 +0500

    Changed icons to tabler

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
     <script src="{{ asset('assets/vendor/fullcalendar/js/main.min.js') }}"></script>
     <script src="{{ asset('assets/js/plugins-init/fullcalendar-init.js') }}"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.47.0/tabler-icons.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
